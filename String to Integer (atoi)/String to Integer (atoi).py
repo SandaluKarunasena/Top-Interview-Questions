aa16fad85662bb04acf38132dfa07df40cd86553
SandaluKarunasena <sandalu.18@cse.mrt.ac.lk>
2025-03-19 16:07:54 +0530

    Atoi

--- String to Integer (atoi)/String to Integer (atoi).py
+++ String to Integer (atoi)/String to Integer (atoi).py
@@ -1,1 +1,18 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        newStr=""
+        for i in s:
+            if i.isdigit():
+                newStr+=i
+            elif (newStr == '') and (i == "+" or i == "-"):
+                newStr+=i
+            else:
+                break
 
+        if newStr == '' or newStr in '-+':
+            return 0
+
+        result = max(-2**31, min(int(newStr), 2**31 - 1))
+
+        return int(result)
